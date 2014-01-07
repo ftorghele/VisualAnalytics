3a0a4f9c7dda36808704c5b415059af25b089b8a
Franz Torghele, BSc <franz@torghele.at>
2014-01-07 11:53:33 +0100

    crawler finished

---
+++ crawler/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'CONSUMER_KEY':        '6l4SqlvVCGmO6plPI9fEpQ',
+  'CONSUMER_SECRET':     'Mj1o7sevRt6a2gcoZKKK9vjcEeadJIgDXfEDmen0',
+  'ACCESS_TOKEN':        '72298424-es2AwX1mtXSYVT4vF1HwE6gMoj3klSZUCA08jTSJM',
+  'ACCESS_TOKEN_SECRET': 'JruI68jVXr25Icsq6MhzGzHl3i0LMnQwwVs9udKFlc450',
+  'MONGODB_DB_URL':      'local',
+  'MONGODB_COLLECTIONS': ['tweets', 'va', 'slang_replaced', 'sentiment', 'country_sentiment'],
+};
