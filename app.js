073c60dfa91daba3eed19ca4ea1c17fbe1600764
Franz Torghele, BSc <franz@torghele.at>
2014-01-07 16:29:37 +0100

    twitter streaming

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -29,8 +28,38 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+var config = require('./config.js');
+var Twit = require('twit');
+var io = require('socket.io');
+
+//Start a Socket.IO listen
+var sockets = io.listen(9000);
+ 
+//Set the sockets.io configuration.
+//THIS IS NECESSARY ONLY FOR HEROKU!
+sockets.configure(function() {
+  sockets.set('transports', ['xhr-polling']);
+  sockets.set('polling duration', 10);
+});
+
+var twitter = new Twit({
+    consumer_key: config.CONSUMER_KEY
+  , consumer_secret: config.CONSUMER_SECRET
+  , access_token: config.ACCESS_TOKEN
+  , access_token_secret: config.ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream('statuses/filter', { track: [':)', ':(', ':D', ':p', 'xD', ':))', ':((', ':-)', ':-(', ":'(" ] });
+
+sockets.sockets.on('connection', function(socket){
+  stream.on('tweet', function (tweet) {
+    if (tweet.text != null && tweet.place != null && tweet.place.country_code != null) {
+      socket.emit("tweet", JSON.stringify({ text: tweet.text, country: tweet.place.country_code}) );  
+    }
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Data Engineering: Task 3' });
 };
+
+
